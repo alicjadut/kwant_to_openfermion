5e3b1078545cd1c8c8ffd0d5cf27c287303bf88d
alicjadutgit config --global user.name alicjadutgit config --global user.name :alicjadut <ala@dutkiewicz.waw.pl>
2020-11-23 18:11:05 +0100

    no need to pass the hoppings iterator

--- kwant_to_openfermion.py
+++ kwant_to_openfermion.py
@@ -1,17 +1,18 @@
 import openfermion
 
 
-def builder_to_FermionOperator(syst, hoppings_iterator):
+def builder_to_FermionOperator(syst):
+    
     ham = openfermion.FermionOperator()
 
     #on site terms
     for ix in syst.id_by_site.values():
         ham = ham + openfermion.FermionOperator(f'{ix}^ {ix}', syst.hamiltonian(ix, ix))
 
-    #hopping terms    
-    for s1, s2 in hoppings_iterator:
-        ix1 = syst.id_by_site[s1]
-        ix2 = syst.id_by_site[s2]
-        hop = openfermion.FermionOperator(f'{ix1}^ {ix2}', syst.hamiltonian(ix1, ix2))
-        ham = ham + hop + openfermion.hermitian_conjugated(hop)
+    #hopping terms
+    for edge in range(syst.graph.num_edges):
+        ix1 = syst.graph.head(edge)
+        ix2 = syst.graph.tail(edge)
+        ham = ham + openfermion.FermionOperator(f'{ix1}^ {ix2}', syst.hamiltonian(ix1, ix2))
+        
     return(ham)
