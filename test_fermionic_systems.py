147033d3e26df5c87cbc7235ee072aaa86edfeec
alicjadutgit config --global user.name alicjadutgit config --global user.name :alicjadut <ala@dutkiewicz.waw.pl>
2021-01-15 23:12:23 +0100

    tests for hubbard interaction added + formatting changed

--- test_fermionic_systems.py
+++ test_fermionic_systems.py
@@ -9,15 +9,35 @@ import fermionic_systems as fs
 sigma_0 = numpy.array([[1., 0], [0, 1]])
     
 def test_single_term_spinless():
-    op1 = fs._single_term_to_FermionOperator(1, 0, 0, fs.Indexer())
-    op2 = openfermion.FermionOperator('0^ 0')
-    assert op1 == op2
+    op = fs._single_term_to_FermionOperator(1, 0, 0, fs.Indexer())
+    op_of = openfermion.FermionOperator('0^ 0')
+    assert op == op_of
 
 def test_single_term_spin():
-    op1 = fs._single_term_to_FermionOperator(sigma_0, 0, 0, fs.Indexer())
-    op2 = openfermion.FermionOperator('0^ 0')+openfermion.FermionOperator('1^ 1')
-    assert op1 == op2
+    op = fs._single_term_to_FermionOperator(sigma_0, 0, 0, fs.Indexer())
+    op_of = openfermion.FermionOperator('0^ 0')+openfermion.FermionOperator('1^ 1')
+    assert op == op_of    
+
+def test_indexer():
+    ind = fs.Indexer()
+    ind.index('a')
+    ind.index('b')
     
+    assert ind.element(0) == 'a'
+    assert ind.index('b') == 1
+
+def test_hubbard_interaction():
+    ind = fs.Indexer()
+    ind.index((0,0,2))
+    ind.index((0,1,2))
+    
+    ham_int = fs.hubbard_interaction(1, ind)
+    
+    ham_int_of = openfermion.FermionOperator('0^ 0 1^ 1')
+    
+    assert ham_int == ham_int_of
+    
+
 def test_chain():
 
     a = 1
@@ -32,20 +52,24 @@ def test_chain():
     ##Hopping terms
     syst[lat.neighbors()] = -t
     syst = syst.finalized()
+    ham, ind = fs.system_to_FermionOperator(syst, return_indexer = True)
 
     #Build a hamiltonian in openfermion
-    ham = openfermion.FermionOperator()
+    ham_of = openfermion.FermionOperator()
     ##On-site terms
     for i in range(L):
-        ham += openfermion.FermionOperator(f'{i}^ {i}', t)
+        ham_of += openfermion.FermionOperator(f'{i}^ {i}', t)
     ##Hopping terms
     for i in range(L-1):
         term = openfermion.FermionOperator(f'{i}^ {i+1}', -t)
-        ham += term
-        ham += openfermion.hermitian_conjugated(term)
-   
+        ham_of += term
+        ham_of += openfermion.hermitian_conjugated(term)
+
     #Test if equal
-    assert fs.system_to_FermionOperator(syst) == ham
+    assert ham == ham_of
+    
+    #Hubbard interaction
+    assert fs.hubbard_interaction(1, ind) == openfermion.FermionOperator()
 
 def test_spin_hubbard():
     '''
@@ -55,6 +79,7 @@ def test_spin_hubbard():
     a = 1
     L = 2
     t = 1
+    U = 3
 
     #Build an LxL square lattice in kwant
     lat = kwant.lattice.square(a)
@@ -63,12 +88,15 @@ def test_spin_hubbard():
     ##Hopping terms between neighbours with the same spin
     syst[lat.neighbors()] = -t * sigma_0
     syst = syst.finalized()
+    ham, ind = fs.system_to_FermionOperator(syst, return_indexer = True)
+    #On-site interactions
+    ham += fs.hubbard_interaction(U, ind)
 
     #Build a hamiltonian in openfermion
-    ham = openfermion.hamiltonians.fermi_hubbard(L, L, t, 0, spinless=False)
+    ham_of = openfermion.hamiltonians.fermi_hubbard(L, L, t, U, spinless=False)
   
     #Test if equal
-    assert fs.system_to_FermionOperator(syst) == ham
+    assert ham == ham_of
     
 def test_different_spins():
     '''
@@ -82,12 +110,14 @@ def test_different_spins():
     for i in range(n_sites):
         syst[lat(i)] = numpy.diag(numpy.ones(i+1))
     syst = syst.finalized()
+    ham, ind = fs.system_to_FermionOperator(syst, return_indexer = True)
     
-    ham = openfermion.FermionOperator()
+    ham_of = openfermion.FermionOperator()
     for i in range((n_sites*(n_sites+1))//2):
-        ham += openfermion.FermionOperator(f'{i}^ {i}')
+        ham_of += openfermion.FermionOperator(f'{i}^ {i}')
         
-    assert fs.system_to_FermionOperator(syst) == ham
+    assert ham == ham_of    
+    
     
 def test_different_spins_hopping():
     '''
@@ -101,7 +131,7 @@ def test_different_spins_hopping():
     syst[lat(0), lat(1)] = numpy.array([[1., -1.]])
     syst = syst.finalized()
     
-    ham = \
+    ham_of = \
     openfermion.FermionOperator('0^ 0', 2.) + \
     openfermion.FermionOperator('1^ 1', 3.) + \
     openfermion.FermionOperator('2^ 2', 3.) + \
@@ -110,6 +140,6 @@ def test_different_spins_hopping():
     openfermion.FermionOperator('2^ 0', -1.) + \
     openfermion.FermionOperator('0^ 2', -1.)
     
-    assert fs.system_to_FermionOperator(syst) == ham
+    assert fs.system_to_FermionOperator(syst) == ham_of
     
 
